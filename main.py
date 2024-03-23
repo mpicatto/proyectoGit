d18230a4cb4f0ed49e090991484bfe6f6f78888c
Mauro Picatto <mauropicatto@gmail.com>
2024-03-23 18:57:20 -0300

    Primer commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+Consigna 1: 
+a) crear una nueva rama
+bCrear en la carpera modules un archovo con el nombre del alumno y dentro del mismo crear una función.
+c)Importar la funcion en este archivo y llamarla.
+d) subir la rama al repositorio remoto
+Consigna 2: 
+a) crear una nueva rama
+b)Crear en el archivo creado en modules una clase. 
+c)importar la clase en este archivo y crear unas instancia de la misma.
+d) subir la rama al repositorio remoto
+'''
